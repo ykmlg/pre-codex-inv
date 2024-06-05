14193894b87190843ad75b1b4d983a74e4fabd15
Tanjil <tanjilkh@gmail.com>
2024-06-05 16:13:04 +0100

    Update precodex.sh

--- precodex.sh
+++ precodex.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# Example 
+# codex_15="myusername"
+# echo #codex_15
+# Run the script with: bash precodex.sh
+# If you are on windows, use cygwin
+# https://www.cygwin.com/
+
 codex_01="tskxz"
 codex_02="johnx"
 codex_03="takken"
