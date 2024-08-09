5dfd16ac62fef6224999255b7fd9ffd987e4422d
ariel <arielshokrany04@gmail.com>
2024-08-09 17:35:27 +0300

    updated

--- precodex.sh
+++ precodex.sh
@@ -12,10 +12,20 @@ codex_02="johnx"
 codex_03="takken"
 codex_04="userx"
 codex_05="usery"
-codex_06="alch"
+codex_06="Frante"
+codex_07="cosmo"
+codex_08="PhantomBunny"
+codex_09="Whisper-source"
+codex_10="GG"
+codex_11="alch"
+
 echo $codex_01
 echo $codex_02
 echo $codex_03
 echo $codex_04
 echo $codex_05
-echo $codex_06
+echo $codex_07
+echo $codex_08
+echo $codex_09
+echo $codex_10
+echo $codex_11
