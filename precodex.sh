c0d5a8fad041816c267142a34da6bba8c17fe6bd
zby1211 <54210771+zby1211@users.noreply.github.com>
2024-08-17 21:59:35 +0800

    Update precodex.sh
    Added user Ca1c.

--- precodex.sh
+++ precodex.sh
@@ -17,6 +17,8 @@ codex_07="cosmo"
 codex_08="PhantomBunny"
 codex_09="Whisper-source"
 codex_10="GG"
+codex_11="Ca1c"
+
 
 echo $codex_01
 echo $codex_02
@@ -27,3 +29,4 @@ echo $codex_07
 echo $codex_08
 echo $codex_09
 echo $codex_10
+echo $codex_11
